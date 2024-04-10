d0419c4da323f44a926c3b0a27ee99ee421e5161
Alex Morega <alex.morega@torchbox.com>
2024-04-10 12:21:58 +0300

    Include links in HTML

--- spike/mailchimp/management/commands/mc_create_campaign.py
+++ spike/mailchimp/management/commands/mc_create_campaign.py
@@ -6,6 +6,12 @@ from ...client import create_campaign, get_client, log_api_errors
 class Command(BaseCommand):
     help = "Create a campaign"
 
+    HTML = """
+        <p>HELLO THERE <strong>html</strong>.</p>
+        <p><a href="https://torchbox.com">click me!</a></p>
+        <p><a href="*|UNSUB|*">unsubscribe</a></p>
+    """
+
     def add_arguments(self, parser):
         parser.add_argument("list_id")
 
@@ -14,6 +20,5 @@ class Command(BaseCommand):
         client = get_client()
         with log_api_errors(reraise=False):
             campaign = create_campaign(client, recipients={"list_id": list_id})
-            html = "HELLO THERE <strong>html</strong>"
-            client.campaigns.set_content(campaign["id"], {"html": html})
+            client.campaigns.set_content(campaign["id"], {"html": self.HTML})
             print(campaign["id"])
